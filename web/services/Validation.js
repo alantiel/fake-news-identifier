7dbc2f30700bb93a4f4e0f244e2aa8fb6870815d
guilhermeiobbi <guilherme.iobbi@gmail.com>
2017-11-08 17:02:28 -0200

    Including originUrl in articles mock

--- web/services/Validation.js
+++ web/services/Validation.js
@@ -150,7 +150,7 @@ var articles = [
         url: 'http://cnnn.com/news/1',
         imgCloud:'https://cloud.google.com/vision/images/rushmore.jpg',
         imgUrl: 'https://conteudo.imguol.com.br/c/noticias/67/2017/11/08/08nov2017---comandante-geral-da-gcm-de-sp-adelson-de-souza-1510158156286_300x420.jpg',
-        originUrl:''
+        originUrl:'https://noticias.uol.com.br/cotidiano/ultimas-noticias/2017/11/08/comandante-da-guarda-civil-de-sao-paulo-diz-que-bairro-de-um-dos-fatores-que-influenciam-abordagens-policiais.htm'
     },    
     {
         title: 'A chocante foto de elefantes em chamas premiada em concurso!!!!',
@@ -158,7 +158,7 @@ var articles = [
         url: 'http://cnnn.com/news/1',
         imgCloud:'https://cloud.google.com/vision/images/rushmore.jpg',
         imgUrl: 'https://conteudo.imguol.com.br/c/noticias/3c/2017/11/08/a-imagem-de-dois-elefantes-fugindo-de-uma-multidao-que-ateava-fogo-neles-foi-escolhida-como-a-vencedora-de-um-premio-de-fotografia-da-vida-selvagem-1510158759882_615x300.jpg',
-        originUrl:''
+        originUrl:'https://noticias.uol.com.br/meio-ambiente/ultimas-noticias/bbc/2017/11/08/a-chocante-foto-de-elefantes-em-chamas-premiada-em-concurso.htm'
     }
 ]
 
@@ -196,7 +196,7 @@ module.exports = function() {
         validations.push(isValidStep10)
 
         article['validations'] = validations;
-        article['score'] = 100 - validations.filter((validation) => validation.valid === true).length * 10;
+        article['score'] = validations.filter((validation) => validation.valid === false).length * 10;
     });
     console.log(articles);
     return articles;
