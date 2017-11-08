8f9c42247128ceb9cf200921ee82542ca46c06c0
guilhermeiobbi <guilherme.iobbi@gmail.com>
2017-11-08 17:02:14 -0200

    Adding route 'rest/news'

--- web/app.js
+++ web/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var news = require('./routes/news');
+var restNews = require('./routes/rest-news');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/news', news);
+app.use('/rest/news', restNews);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ web/routes/rest-news.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var validationService = require('../services/Validation');
+var router = express.Router();
+ 
+router.get('/', function(req, res, next) {
+    var url = req.query.url;
+    
+    var articles = validationService();
+    var score = articles.filter(article => article.originUrl === url)
+    .map(article => article.score)
+    score = score.length > 0 ? score[0] : 0;
+    res.send({
+        score: score
+    });
+});
+
+module.exports = router;
