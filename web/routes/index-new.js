f1f7545fc2d5e97f0b1e2bc9e1714b266ad18c40
AntonioCesar <jrcesar4@gmail.com>
2017-11-11 23:03:15 -0200

    First version

--- web/routes/index-new.js
+++ web/routes/index-new.js
@@ -1,10 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const validator = require('../services/Validator');
+
+const articles = require('../input/articles').sample;
 
 router.get('/', (req, res) => {
-		console.log('Index New Porra')
+
+	const fakeArticle = articles[1]; // real article = [0]
+	const responseValidations = validator.validate([fakeArticle])[0];
+	const validations = responseValidations.validations;
+  
+	const resHeadLine = validations[0];
+	const resURL = validations[1];
+	const resReputation = validations[2];
+	const resFormatting = validations[3];
+	const resImage = validations[4];
+	const resDate = validations[5];
+	const resSeven = validations[6];
+	const resOtherSources = validations[7];
+	const resJoke = validations[8];
+	const resTen = validations[9];
+
+	console.log("teste", responseValidations.countFalse);
+	
 	  res.render('indexnew', { 
-        title: 'Fake News Identifier'
+        title: 'Fake News Identifier',
+        article: fakeArticle,
+        validationOne: resHeadLine,
+        validationTwo: resURL,
+        validationThree: resReputation,
+        validationFour: resFormatting,
+        validationFive: resImage,
+        validationSix: resDate,
+        validationSeven: resSeven,
+        validationEight: resOtherSources, 
+        validationNine: resJoke,
+        validationTen: resTen,
+		countFalse: responseValidations.countFalse,
+		countTrue: responseValidations.countTrue,
+		countUnknown: responseValidations.countUnknown,
+        finalRate: 'Probability of being fake news: '.concat(responseValidations.score).concat('%')
     });
 
 });
