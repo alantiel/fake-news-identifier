7b7eb9f8dde3c085b98f220538b4baa2545dba8f
Guilherme Iobbi <guilherme.iobbi@gmail.com>
2017-11-12 22:33:55 -0200

    inclusao de integracao e validacao com o watson

---
+++ web/config/IBM_WATSON_API_CONFIG.js
@@ -0,0 +1,8 @@
+// Personality Insights
+const PI_ENDPOINT = 'https://gateway.watsonplatform.net/personality-insights/api/v3/profile?version=2017-10-13';
+const PI_USERNAME = '206ae7ee-5c30-4962-9a5f-4331072a94e8'; //giobbi
+const PI_PASSWORD = 'G1vhCszBsXlZ'; //giobbi
+
+module.exports = {
+    PI_ENDPOINT, PI_USERNAME, PI_PASSWORD
+}
