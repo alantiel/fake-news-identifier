1420f877cc18f856cce4827fdcf875889a5ae1f9
AntonioCesar <jrcesar4@gmail.com>
2017-11-15 20:17:39 -0200

    validate evidence

--- web/services/ValidationEvidence.js
+++ web/services/ValidationEvidence.js
@@ -3,12 +3,12 @@ const localStorage = require('localStorage')
 const validationEvidence = function(url){
 
 	let rate = parseInt(localStorage.getItem('count' + url))
-
+	var variables = {rate: rate}
 	if(isNaN(rate) || rate == 0) {
-	    return {pass: 'unknown'};
+			return {pass: 'unknown', variables: variables};
 	}
 	else {
-        return {pass:rate > 0};
+      return {pass:rate > 0, variables: variables};
     }
 };
 
