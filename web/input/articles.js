63c5d9eec59636f3b121616e3c3e7fb333805f45
cmsandiga <cmsandiga@gmail.com>
2017-11-15 23:28:00 -0200

    update

--- web/input/articles.js
+++ web/input/articles.js
@@ -4,8 +4,8 @@ const articles = [
         desc: 'An arresting image showing an adult elephant and its calf fleeing a mob attack has won a top Asian wildlife photography prize. It shows the two animals running among a crowd that has hurled flaming tar balls and crackers at them, reportedly to ward the elephants away from human settlements. The picture, titled “Hell is here”, was taken by Biplab Hazra, a wildlife photographer from West Bengal state, and won the 2017 Sanctuary’s Wildlife Photographer of the Year award. The Sanctuary Nature Foundation, which awarded the prize, said: “In the Bankura district of West Bengal, this sort of humiliation of pachyderms is routine, as it is in the other elephant-range states of Assam, Odisha, Chhattisgarh, Tamil Nadu and more. “This sort of conflict is increasing every day,” said Christy Williams, the World Wildlife Fund country director in Myanmar, who researches elephants in the region. He said elephants were increasingly being pushed out of existing habitats by human behaviour. “There are forests being cut down, degraded, and also being fragmented by development like new roads and pipelines.” India is home to around 30,000 Asian elephants, 70% of the world’s population, with around 800 in West Bengal, according to the most recent official count. Co-existence between humans and elephants was especially difficult, Williams said. “Elephants are huge – they are the biggest mammal on land and they have huge home ranges, around 800 sq km. Such huge unreserved forest tracts are becoming very rare,” he said. “In the end, humans always win, whatever the species, however powerful it is.”',
         url: 'https://theguardian.com/world/2017/nov/07/photo-of-elephant-and-calf-fleeing-fire-throwing-mob-wins-top-prize',
 		date: 'Nov 10, 2017',
-        imgCloud:'https://storage.googleapis.com/fakenews_hackaton/elefante.jpg',
-        imgUrl: 'https://storage.googleapis.com/fakenews_hackaton/elefante.jpg',
+        imgCloud:'https://storage.googleapis.com/fake-news/elefante.jpg',
+        imgUrl: 'https://storage.googleapis.com/fake-news/elefante.jpg',
         originUrl:'https://theguardian.com/world/2017/nov/07/photo-of-elephant-and-calf-fleeing-fire-throwing-mob-wins-top-prize'
     },
     {
@@ -13,8 +13,8 @@ const articles = [
         desc: 'Renowned radio show host and fake news creator Alex Jones has reportedly asked his audience to fly the false flag at half-staff today in honor of the Texas shooting victims.  Jones’ request comes after a gunman opened fire on the parishioners of the First Baptist Church of Sutherland Springs, Texas killing more than 20 and injuring dozens more.  While details of the shooter have not been released, Alex is likely spinning up all kinds of untruths in order to satisfy his legion of brain-dead followers. “Was the shooter a Muslim?  Antifa?  BLM?  Was he radicalized by the gay agenda?  Why did he hate Christians?  Is there really even a church in Sutherland Springs? Are these the same crisis actors used during the Vegas shooting?  Did you see that Craigslist ad recently seeking extras for a mass casualty exercise? How was Hillary involved? Could Obama have done more to prevent this attack?  Did some crazed liberal shoot up this church to blame Trump and take away our guns? If this story is even true why did none of the parishioners shoot back?,” Alex likely opined during this afternoons editorial meeting.',
         url: 'http://nationalreport.net/alex-jones-suggests-flying-false-flag-half-staff-texas-church-shooting-victims/',
         date: 'Jan 6, 2017',
-        imgCloud:'http://nationalreport.net/wp-content/uploads/2017/11/8368024532_d74e9d988d_z.jpg',
-        imgUrl: 'http://nationalreport.net/wp-content/uploads/2017/11/8368024532_d74e9d988d_z.jpg',
+        imgCloud:'https://storage.googleapis.com/fake-news/nationalreport.jpg',
+        imgUrl: 'https://storage.googleapis.com/fake-news/nationalreport.jpg',
         originUrl:'http://nationalreport.net/alex-jones-suggests-flying-false-flag-half-staff-texas-church-shooting-victims/'  
     }
     ,
@@ -23,8 +23,8 @@ const articles = [
         desc: 'Two former US intelligence chiefs say Donald Trump\'s stance on Russian meddling in last year\'s presidential election is putting the US at risk.',
         url: 'http://bbc.com/news/world-us-canada-41962023',
         date: 'Nov 12, 2017',
-        imgCloud:'https://ichef.bbci.co.uk/news/660/cpsprodpb/D797/production/_98719155_mediaitem98719154.jpg',
-        imgUrl: 'https://ichef.bbci.co.uk/news/660/cpsprodpb/D797/production/_98719155_mediaitem98719154.jpg',
+        imgCloud:'https://storage.googleapis.com/fake-news/trump.jpg',
+        imgUrl: 'https://storage.googleapis.com/fake-news/trump.jpg',
         originUrl:'http://bbc.com/news/world-us-canada-41962023'  
     }
     ,
@@ -33,8 +33,8 @@ const articles = [
         desc: 'A large homeless encampment was cleared in Denver on Thursday amid temperatures of -5C (23F), risking further controversy over the city\’s approach to homeless people struggling with winter weather.',
         url: 'https://theguardian.com/society/2017/jan/26/denver-officials-clear-homeless-camp-controversy-winter-elements',
         date: 'Jan 26, 2017',
-        imgCloud:'https://i.guim.co.uk/img/media/ae8d332508e7278f5e77ad45b9023bfb89aa9f0a/0_253_3264_1958/master/3264.jpg?w=620&q=55&auto=format&usm=12&fit=max&s=b165c5488bb49c28ad576baa0512da16',
-        imgUrl: 'https://i.guim.co.uk/img/media/ae8d332508e7278f5e77ad45b9023bfb89aa9f0a/0_253_3264_1958/master/3264.jpg?w=620&q=55&auto=format&usm=12&fit=max&s=b165c5488bb49c28ad576baa0512da16',
+        imgCloud:'https://storage.googleapis.com/fake-news/michigan.jpg',
+        imgUrl: 'https://storage.googleapis.com/fake-news/michigan.jpg',
         originUrl:'https://theguardian.com/society/2017/jan/26/denver-officials-clear-homeless-camp-controversy-winter-elements'  
     }
 ];
